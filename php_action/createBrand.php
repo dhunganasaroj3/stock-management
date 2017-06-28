67c17c0c5c25c08d3c77b1aae605709de68da0b3
Binod Nagarkoti <binod1365@gmail.com>
2017-06-28 21:55:10 +0545

    Update createBrand.php
    Added the SQL Statement to SELECT the BrandName to check if The Brand Name is already in database

--- php_action/createBrand.php
+++ php_action/createBrand.php
@@ -1,26 +1,39 @@
 <?php 	
 
 require_once 'core.php';
-
 $valid['success'] = array('success' => false, 'messages' => array());
 
 if($_POST) {	
-
 	$brandName = $_POST['brandName'];
-  $brandStatus = $_POST['brandStatus']; 
+	$brandStatus = $_POST['brandStatus']; 
+	$check_sql = "SELECT * FROM brands WHERE brand_name = '$brandName' ";
+	$result = $connect->query($check_sql);
+	if (!$result) {
+		echo ('A database error occurred in processing your '.'submission.\nIf this error persists, please '.'contact binod1365@gmail.com.');
+	}
+		if ($result->num_rows === 0){
+		$sql = "INSERT INTO brands (brand_name, brand_active, brand_status) VALUES ('$brandName', '$brandStatus', 1)";
 
-	$sql = "INSERT INTO brands (brand_name, brand_active, brand_status) VALUES ('$brandName', '$brandStatus', 1)";
-
-	if($connect->query($sql) === TRUE) {
-	 	$valid['success'] = true;
-		$valid['messages'] = "Successfully Added";	
-	} else {
-	 	$valid['success'] = false;
-	 	$valid['messages'] = "Error while adding the members";
+		if($connect->query($sql) === TRUE) {
+	 		$valid['success'] = true;
+			$valid['messages'] = "Successfully Added";	
+			} else {
+	 				$valid['success'] = false;
+				$valid['messages'] = "Error while adding the members";
+		 }
+	}
+	if ($result->num_rows == 1){
+			$valid["success"] = false;
+			$valid["messages"] = $value."Data already in databas .Please check into your database.Choose other!!";
+		
 	}
 	 
+
 	$connect->close();
 
 	echo json_encode($valid);
  
 } // /if $_POST
+
+
+?>
